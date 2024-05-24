3877eceeb4c645effc2786e23354ed883ea6c374
Xiaonuo Gantan <xiaonuo.gantan@gmail.com>
2024-05-24 16:00:41 -0400

    Add `fn check_existence`

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,11 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    check_existence(&input.trim());
+}
+
+/** Check if the input cmd exists in the predefined list of commands of the shell */
+fn check_existence(input: &str) -> () {
+    println!("{}: command not found", input)
 }

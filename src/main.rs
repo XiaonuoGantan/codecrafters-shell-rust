3f4fdceec94b7beecb5d33a387c13b96c1c6a9ff
Xiaonuo Gantan <xiaonuo.gantan@gmail.com>
2024-05-24 16:30:48 -0400

    Implement `type` with find-existing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
 
 fn main() {
     let stdin = io::stdin();
@@ -14,7 +15,7 @@ fn main() {
     }
 }
 
-/** 
+/**
  * Try to run the command `input` if it's predefined, otherwise print an error message.
  */
 fn run(input: &str) -> () {
@@ -22,11 +23,31 @@ fn run(input: &str) -> () {
     match words[0] {
         "exit" => std::process::exit(words[1].parse().unwrap()),
         "echo" => println!("{}", words[1..].join(" ")),
-        "type" => if ["exit", "echo", "type"].contains(&words[1]) {
-            println!("{} is a shell builtin", words[1])
-        } else {
-            println!("{} not found", words[1])
-        },
-        _ => println!("{}: command not found", input)
+        "type" => {
+            if ["exit", "echo", "type"].contains(&words[1]) {
+                println!("{} is a shell builtin", words[1])
+            } else {
+                if !check_path(words[1]) {
+                    println!("{} not found", words[1])
+                }
+            }
+        }
+        _ => println!("{}: command not found", input),
+    }
+}
+
+fn check_path(cmd: &str) -> bool {
+    match (env::var("PATH"), cmd) {
+        (Ok(paths), cmd) => {
+            for path in paths.split(':') {
+                let path = format!("{}/{}", path, cmd);
+                if std::path::Path::new(&path).exists() {
+                    println!("{} is in {}", cmd, path);
+                    return true;
+                }
+            }
+            false
+        }
+        _ => false,
     }
 }
